30d20d957cf6993dacdc6a4c9a752a9a38bc7e18
Nduka Konyhea Prince <ndukaprincek@gmail.com>
2024-03-13 20:30:55 -0700

    4-am and still more bugs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
-print("Last digit of {number}",end= " ")
+print(f"Last digit of {number}", end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
-elif last_digit < 6 and last_digit !=  0:
+elif last_digit < 6 and last_digit != 0:
     print("and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-        print(f"i = 0x{i:01x}")
+    print("i = 0x{:01x}".format(i))
