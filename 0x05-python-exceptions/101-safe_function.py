34f5f823ce35323ad440feb52bb43040ce3a2c34
konyhea <ndukaprincek@gmail.com>
2024-04-05 01:51:46 +0100

    done

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+import sys
+
+def fct(a, b):
+    return a / b
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
+    
