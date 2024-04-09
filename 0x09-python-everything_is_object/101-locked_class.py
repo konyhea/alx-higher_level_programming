b7bd2b7cf9491a6372716bce93176d0b7d8d925a
konyhea <ndukaprincek@gmail.com>
2024-04-10 00:55:32 +0100

    fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,10 @@
+#!/usr/bin/python3
 class LockedClass:
     __slots__ = "first_name"
+
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
