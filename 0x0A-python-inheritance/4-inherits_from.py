c58f52a156c9c9314c60775f7a80b09255b06188
konyhea <ndukaprincek@gmail.com>
2024-04-18 02:45:43 +0100

    work

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """inherited directly or indirectly"""
-    return issubclass(type(obj), a_class)  and type(obj) != a_class
+    return issubclass(type(obj), a_class) and (type(obj) != a_class)
