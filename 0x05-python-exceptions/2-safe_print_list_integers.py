7ba7cd0969d1bfff3a3509c1da81c87b1de48a7d
konyhea <ndukaprincek@gmail.com>
2024-04-22 23:28:54 +0100

    f

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 counter += 1
     except (ValueError, TypeError):
         pass
-
+    except IndexError:
+        raise Exception
     finally:
         print()
-
     return counter
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
+    result = []
     try:
-        result = [i / j for i, j in zip(my_list_1, my_list_2)]
         for i, j in zip(my_list_1, my_list_2):
             if not isinstance(i, (float| int)) or not isinstance(j, (float | int)):
                 result.append(0)
-        if j == 0:
-            result.append(0)
+                continue
+            if j == 0:
+                result.append(0)
+                continue
+            result.append(i/j)
     except ZeroDivisionError:
         print("division by 0")
     except TypeError:
         print("wrong type")
     except IndexError:
         print("out of range")
-    
     finally:
-        for i in range(list_length):
-            return result
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-        
+        return result[:list_length]
