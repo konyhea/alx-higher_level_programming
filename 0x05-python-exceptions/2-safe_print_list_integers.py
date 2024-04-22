242418dfd6fa8d7529e72f701a14cdc5a515f688
konyhea <ndukaprincek@gmail.com>
2024-04-22 23:52:07 +0100

    fr

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                counter += 1
-    except (ValueError, TypeError):
-        pass
-    except IndexError:
-        raise Exception("list index out of range")
-    finally:
-        print()
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except (ValueError, TypeError):
+            pass
+    print()
     return counter
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,20 +2,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    try:
-        for i, j in zip(my_list_1, my_list_2):
-            if not isinstance(i, (float| int)) or not isinstance(j, (float | int)):
-                result.append(0)
-                continue
-            if j == 0:
-                result.append(0)
-                continue
-            result.append(i/j)
-    except ZeroDivisionError:
-        print("division by 0")
-    except TypeError:
-        print("wrong type")
-    except IndexError:
-        print("out of range")
-    finally:
-        return result[:list_length]
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            result.append(div)
+    return result
