093fae93408f016ffbb0617b810860661cc4bf72
konyhea <ndukaprincek@gmail.com>
2024-04-28 00:00:06 +0100

    f

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,63 +1,41 @@
 #!/usr/bin/python3
+'''Node for a singly linked list'''
 
-""" sqaure class with size attributes"""
 
-
-class Square:
-    """
-    class that defines a square.
-
-    Attributes
-    -------------
-    size: a private instance attribute
-    """
-    def __init__(self, size=0, position=(0, 0)):
-        """ initializing new square
-        size: the size of the sqauare formed
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
-
-    def area(self):
-        """ returns the area of the square"""
-        return self.__size ** 2
+class Node:
+    def __init__(self, data, next_node=None):
+        '''constructor'''
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def size(self):
-        """ property retriever """
-        return self.__size
+    def data(self):
+        '''retrieve data'''
+        return self.__data
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+    @data.setter
+    def data(self, value):
+        '''data setter'''
+        if isinstance(value, int) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
 
     @property
-    def position(self):
-        return self.__position
+    def next_node(self):
+        '''retrieve next node'''
+        return self.__next_node
 
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise ValueError("position must be a tuple of 2\
-                 positive integers")
-        for args in value:
-            if type(args) is not int or args < 0:
-                raise ValueError("position must be a tuple of 2 \
-                    positive integers")
-        self.__position = value
+    @next_node.setter
+    def next_node(self, value):
+        ''''next node setter'''
+        if isinstance(value, None) is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
-    def my_print(self):
-        y = '_' * self.__position[0]
-        if self.__size == 0:
-            print()
-        for _ in range(self.__size):
-            print(y + '#' * self.__size)
-        return
+class SinglyLinkedList:
+    def __init__(self):
+        '''constructor'''
+        self.__head = None
+
+    def sorted_insert(self, value):
+        '''insert Node'''
