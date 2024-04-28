8ceae5501c49a118a936372ebea44b7c926cc3d7
konyhea <ndukaprincek@gmail.com>
2024-04-28 03:43:25 +0100

    f

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -32,6 +32,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     def __init__(self):
         '''constructor'''
@@ -39,3 +40,26 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         '''insert Node'''
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+                new.next_node = tmp.next_node
+                tmp.next_node = new
+
+    def __str__(self):
+        '''print singly linked list'''
+        data = []
+        tmp = self.__head
+        while tmp is not None:
+            data.append(str(tmp.data))
+            tmp = tmp.next_node
+            return ('\n'.join(data))
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,19 +45,19 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise ValueError("position must be a tuple of 2\
-                 positive integers")
-        for args in value:
-            if type(args) is not int or args < 0:
-                raise ValueError("position must be a tuple of 2 \
-                    positive integers")
+        if not isinstance(value, tuple) or len(value) != 2 or \
+            not all(isinstance(num, int) for num in value) or \
+                not all(num >= 0 for num in value()):
+            raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
-        y = '_' * self.__position[0]
+        '''print the # character'''
         if self.__size == 0:
-            print()
-        for _ in range(self.__size):
-            print(y + '#' * self.__size)
-        return
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
