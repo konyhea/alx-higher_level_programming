d1069acc0d3528c8d41be4b9d8e120e5b27c5f6f
konyhea <ndukaprincek@gmail.com>
2024-05-24 03:41:59 +0100

    f

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Single SELECT statement with LEFT JOIN and ORDER BY clause
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
 LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
