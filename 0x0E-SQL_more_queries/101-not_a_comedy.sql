1236b807a0794c404f3314f5faa312c122956816
konyhea <ndukaprincek@gmail.com>
2024-05-25 23:39:13 +0100

    f

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,7 @@
 -- Hate comedy let fight
-SELECT tv_shows.title
+SELECT DISTINCT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_show_genres.show_id
-    FROM tv_show_genres
-    WHERE tv_show_genres.genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-    )
-)
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name IS NULL OR tv_genres.name != 'Comedy'
 ORDER BY tv_shows.title ASC;
