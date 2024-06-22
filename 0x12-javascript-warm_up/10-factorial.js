596ade93029457c834cfd152fbc5b05d93676c31
konyhea <ndukaprincek@gmail.com>
2024-06-22 03:40:24 +0100

    xx

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 function factorial (a) {
-  if (isNaN(a)) {
+  const num = parseInt(a, 10);
+  if (isNaN(num) || num <= 1) {
     return 1;
   }
-  if (a <= 1) {
-    return 1;
-  }
-  return a * factorial(a - 1);
+  return num * factorial(num - 1);
 }
 
 const args = process.argv[2];
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  const firstInt = parseInt(a, 10);
-  const secInt = parseInt(b, 10);
+const size = parseInt(process.argv[2], 10);
 
-  console.log(firstInt + secInt);
-}
-
-const length = process.argv.length;
-
-if (length < 4) {
-  console.log('NaN');
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-  add(process.argv[2], process.argv[3]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
