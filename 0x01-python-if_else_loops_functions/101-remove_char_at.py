c5707ff29098269b30a3c4db0fa7112f9be57911
konyhea <ndukaprincek@gmail.com>
2025-03-03 05:35:02 +0100

    using slice instead of loop

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+'''function to remove character at index n'''
 def remove_char_at(str, n):
-    newstr = ""
-    for i in range(len(str)):
-        if i != n:
-            newstr += str[i]
-    return newstr
+    '''remove and print it'''
+    if n < 0 or  n > (len(str)):
+        return str
+    return str[:n] + str[n+1:]
+
