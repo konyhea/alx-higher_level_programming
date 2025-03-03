227703861370a202b576caa4a8f2d771237dc8c8
konyhea <ndukaprincek@gmail.com>
2025-03-03 04:59:29 +0100

    replicating a bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+''' function to mimic byte code presented in the 16 ByteCode challenge'''
 def magic_calculation(a, b, c):
+    '''function to compare argument and return answers'''
     if a < b:
         return c
     elif c > b:
         return a + b
     else:
         return a * b - c
+
