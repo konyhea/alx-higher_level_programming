ea237b63d9c03fb232e205efd9f218726d694399
konyhea <ndukaprincek@gmail.com>
2025-03-07 05:55:07 +0100

    js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,4 @@
-#!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+#!/usr/bin/env node
+//writing a script file to print
+myVar = 'JavaScript is amazing'
+console.log(myVar)
