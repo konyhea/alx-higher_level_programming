b27d55011aec7af49bcea5dd3e77753a2233ca4a
konyhea <ndukaprincek@gmail.com>
2025-07-20 12:15:44 +0100

    added a none

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
-        return None
+        return
     for row in matrix:
         for i, elem in enumerate(row):
             print("{:d}".format(elem), end=" " if i < len(row) - 1 else "\n")
