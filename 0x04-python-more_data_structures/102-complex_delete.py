54f3259de4605c43a4adb566afee3e3561480d6b
konyhea <ndukaprincek@gmail.com>
2025-07-20 15:38:57 +0100

    using dictionary comprehension to solve problems

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,5 @@
+#!/uar/bin/python3
+
 def complex_delete(a_dictionary, value):
     """Function that deletes keys with a specific value in a dictionary"""
     keys_to_delete = [key for key in a_dictionary
