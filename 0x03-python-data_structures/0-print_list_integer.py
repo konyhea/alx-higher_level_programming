f0207b9bd503f256056137a2944cedac2be979db
konyhea <ndukaprincek@gmail.com>
2025-07-20 06:11:17 +0100

    fixing prev task with shiorter and more readable codebase

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+# fnc that prints all integers of a list
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    for x in my_list:
+        print("{}".format(x))
