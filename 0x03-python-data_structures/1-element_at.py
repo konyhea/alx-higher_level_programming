3803522b1c168f0191e3352ce7bea80197a78093
konyhea <ndukaprincek@gmail.com>
2025-07-20 06:23:47 +0100

    infused docstring for function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    """ fnc that retrieves an element from a list like in c"""
+    if idx < 0 or len(my_list) <= idx:
         return None
     else:
         return my_list[idx]
