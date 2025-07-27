8e6fd51b80c7998569821e9e0bc2758f302522dd
konyhea <ndukaprincek@gmail.com>
2025-07-27 17:55:41 +0100

    revitazing everything in cd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """fnc to print the number of elements in a list"""
     counter = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
+        for items in my_list[:x]:
+            print(items, end="")
             counter += 1
-    except IndexError:
+    except Exception:
         pass
     finally:
         print()
