5a9e34a457e88797d3bf018eb5855305b1aee7b9
konyhea <ndukaprincek@gmail.com>
2025-07-27 18:51:44 +0100

    raising specific errors with raise

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        jen = find_sus(8, 9)
-    except NameError as error:
-        raise NameError(message) from error
+    """fnc to raise name error with message"""
+    raise NameError(message)
